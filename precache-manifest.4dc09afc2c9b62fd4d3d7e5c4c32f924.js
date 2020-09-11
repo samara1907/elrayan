cf598ea86706c4048e02bcbf080bc44b9d09064d
samara1907 <67167402+samara1907@users.noreply.github.com>
2020-09-11 17:14:54 +0200

    Updates

---
+++ precache-manifest.4dc09afc2c9b62fd4d3d7e5c4c32f924.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9307f1c3db9f9e048ea49296c622e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de1d83885f1f56c23922",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "769876a50f94d92273fc",
+    "url": "/static/css/main.9bfc5244.chunk.css"
+  },
+  {
+    "revision": "de1d83885f1f56c23922",
+    "url": "/static/js/2.ae18a7ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae18a7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769876a50f94d92273fc",
+    "url": "/static/js/main.0af7e654.chunk.js"
+  },
+  {
+    "revision": "26595f5dc2e2bb18e865",
+    "url": "/static/js/runtime-main.6ee24c4b.js"
+  },
+  {
+    "revision": "2fcce8315469a8670d87f3be6d3f99ac",
+    "url": "/static/media/cam.2fcce831.jpg"
+  },
+  {
+    "revision": "6e4b49e3dd5d12989c0efb92e5b04315",
+    "url": "/static/media/dish.6e4b49e3.jpg"
+  },
+  {
+    "revision": "a7a2160bee5d25df32b41c713fc16867",
+    "url": "/static/media/img_147380.a7a2160b.png"
+  },
+  {
+    "revision": "cc8ca11106bdab96d2f66e8426f099d9",
+    "url": "/static/media/img_logo.cc8ca111.jpg"
+  },
+  {
+    "revision": "8b648c0fbe614f5838749650cf89b742",
+    "url": "/static/media/shop.8b648c0f.png"
+  }
+]);
