4422146e71d12e251ee95a343c070b3670bae2da
samara1907 <67167402+samara1907@users.noreply.github.com>
2020-09-13 20:42:23 +0200

    add to home scren

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './components/Home/Home';
 import './App.css';
 import Particles from 'react-particles-js';
+import AddToHomescreen from 'react-add-to-homescreen';
 
 const ParticlesOptions = {
   particles: {
@@ -11,13 +12,21 @@ const ParticlesOptions = {
         }  
     }
   }
+
+ 
+
 function App() {
   return (
     <div className="App">
+    <AddToHomescreen onAddToHomescreenClick={handleAddToHomescreenClick()} />
     <Particles className='particles' params={ParticlesOptions} />
     <Home/>
     </div>
   );
 }
-
+const handleAddToHomescreenClick = () => {
+  alert(`
+    1. Open Share menu
+    2. Tap on "Add to Home Screen" button`);
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if ('serviceWorker' in navigator) {
+  console.log("Will the service worker register?");
+  navigator.serviceWorker.register('service-worker.js')
+    .then(function(reg){
+      console.log("Yes, it did.");
+    }).catch(function(err) {
+      console.log("No it didn't. This happened: ", err)
+    });
+} 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
