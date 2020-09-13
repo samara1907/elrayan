26d812d27a2b659572f2e474e92dbf2fdf87ddc1
samara1907 <67167402+samara1907@users.noreply.github.com>
2020-09-13 20:00:40 +0200

    service work

---
+++ src/components/button.js
@@ -0,0 +1,14 @@
+import React from './react';
+import './button.css';
+
+const Button =()=>{
+    return(
+        <div>
+            <button class="add-button">Add to home screen</button>
+        </div>
+    );
+}
+
+
+
+export default Button;
