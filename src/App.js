9692ca8b678ea5aacf5c91405c4b6cbcaa037994
samara1907 <67167402+samara1907@users.noreply.github.com>
2020-09-14 17:52:01 +0200

    A2HS

--- src/App.js
+++ src/App.js
@@ -17,31 +17,9 @@ const ParticlesOptions = {
 
 function App() {
   const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
-  const handleClick = () => {
-    pwaInstall({
-      title: "Install Web App",
-      features: (
-        <ul>
-          <li>Cool feature 1</li>
-          <li>Cool feature 2</li>
-          <li>Even cooler feature</li>
-          <li>Works offline</li>
-        </ul>
-      ),
-      description: "This is how the install dialog looks like. Here you can describe your app briefly.",
-    })
-      .then(() => alert("App installed successfully or instructions for install shown"))
-      .catch(() => alert("User opted out from installing"));
-  };
+ 
   return (
     <div className="App">
-      <div>
-      {supported() && !isInstalled() && (
-        <button type="button" onClick={handleClick}>
-          Install App
-        </button>
-      )}
-    </div>
     <Particles className='particles' params={ParticlesOptions} />
     <Home/>
     </div>
