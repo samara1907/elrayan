ac8453fa7573e368003f381208726effeeb4f1b0
samara1907 <67167402+samara1907@users.noreply.github.com>
2020-09-14 17:45:22 +0200

    A2HS

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-if ('serviceWorker' in navigator) {
-  console.log("Will the service worker register?");
-  navigator.serviceWorker.register('service-worker.js')
-    .then(function(reg){
-      console.log("Yes, it did.");
-    }).catch(function(err) {
-      console.log("No it didn't. This happened: ", err)
-    });
-} 
+import ReactPWAInstallProvider from "react-pwa-install";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactPWAInstallProvider enableLogging>
+       <App />
+    </ReactPWAInstallProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -24,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
